7b654fe3027bb65044fb5cd7263a6cf97a7c1cae
tangmeng <meng.tang@livestar.com>
2018-05-23 16:46:26 +0800

    修改打包的CSS路径和打包的输出根路径

--- pirate-pro/build/webpack.base.conf.js
+++ pirate-pro/build/webpack.base.conf.js
@@ -18,9 +18,10 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
+    /*publicPath: process.env.NODE_ENV === 'production'            //开发路径
       ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+      : config.dev.assetsPublicPath*/
+    publicPath:'./'                                           //打包路径
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
